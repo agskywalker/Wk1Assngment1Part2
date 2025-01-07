3dd99df419f25f629e9ca0ec2b0b5c95c2a12e21
agskywalker <saunders7live@gmail.com>
2025-01-07 15:19:39 -0600

    Initial commit

---
+++ src/components/Movies.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Movies() {
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      <p>This page will be developed later.</p>
+    </div>
+  );
+}
+
+export default Movies;
